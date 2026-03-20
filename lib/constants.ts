6272929384295a55092487b487ad415ac9dd8171
Andrew McGary <AmacBFS@gmail.com>
2026-03-20 07:44:08 -0400

    Update shared scoring constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,11 +2,11 @@ export const PLAYER_NAMES = ["Andrew", "Wesley", "Greg", "Eric"] as const;
 
 export const SCORING = {
   "Round of 64": 2,
-  "Round of 32": 4,
-  "Sweet 16": 7,
-  "Elite Eight": 13,
-  "Final Four": 20,
-  Championship: 37,
+  "Round of 32": 5,
+  "Sweet 16": 10,
+  "Elite Eight": 17,
+  "Final Four": 25,
+  Championship: 35,
 } as const;
 
 export const DRAFT_ROUNDS = 16;
